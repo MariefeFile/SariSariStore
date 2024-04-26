6e287ff6180771c281edcede76e7cf75fde73d4a
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-04-26 22:23:04 +0800

    added TableQuery class

---
+++ store/Constants/TableQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store.Constants
+{
+    internal class TableQuery
+    {
+        public static readonly string QueryProducts = "QueryProducts";
+        public static readonly string QueryCustomer = "QueryCustomer";
+    }
+}
