1d2bdbfe84d6f9ab6d1f9ad0e103740df7e88682
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-04-28 11:05:30 +0800

    added Calculations class in Services

---
+++ store/Services/Calculations.cs
@@ -0,0 +1,31 @@
+﻿using store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store.Services
+{
+    internal class Calculations
+    {
+        public static double CalculateTotalPrice(List<OrderItem> orderItems)
+        {
+            double totalPrice = 0;
+            foreach(OrderItem orderItem in orderItems)
+            {
+                totalPrice += orderItem.TotalPrice;
+            }
+            return totalPrice;
+        }
+
+        public static double CalculateItemTotalPrice(OrderItem item)
+        {
+            return item.SellingPrice * item.Quantity;
+        }
+        public static int NewTotalQuantity(int origQuantity, int toAddQuantity)
+        {
+            return origQuantity + toAddQuantity;
+        }
+    }
+}
