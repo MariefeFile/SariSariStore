a41f0292f51b68ac30c1af149470883002a4ab1b
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-04-28 19:28:38 +0800

    added Payment model

---
+++ store/Models/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store.Models
+{
+    public class Payment
+    {
+        public Order order { get; set; }
+        public List<OrderItem> items { get; set; }
+        public double TotalChange { get; set; }
+        public double TotalCash { get; set; }
+        public string EmployeeName { get; set; }
+        public Payment()
+        {
+
+        }
+    }
+}
