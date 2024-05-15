9b49b3729bc7834154ba1fb39be48d12edec8ea9
razstvien01 <nicolenevanz.aricayos@gmail.com>
2024-05-15 23:37:43 +0800

    added type of Sales contants

---
+++ store/Constants/Orders/SelectedSales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store.Constants.Orders
+{
+    public class SelectedSales
+    {
+        public static readonly string DAILY = "Daily";
+        public static readonly string WEEKLY = "Weekly";
+        public static readonly string MONTHLY = "Monthly";
+        public static readonly string YEARLY = "Yearly";
+    }
+}
